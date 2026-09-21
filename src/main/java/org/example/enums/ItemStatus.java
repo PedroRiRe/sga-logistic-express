88dc189c49f45c7a81b6231edd53d188c82154f6
PedroRiRE <enlosojsdepedro@hotmail.com>
2026-09-21 17:47:53 +0200

    feat: añadir enumeraciones ItemStatus y ZoneStatus para el dominio del almacén

---
+++ src/main/java/org/example/enums/ItemStatus.java
@@ -0,0 +1,8 @@
+package org.example.enums;
+
+public enum ItemStatus {
+    DISPONIBLE,
+    RESERVADO,
+    AGOTADO,
+    DANADO
+}
---
+++ src/main/java/org/example/enums/ZoneStatus.java
@@ -0,0 +1,9 @@
+package org.example.enums;
+
+public enum ZoneStatus {
+    LIBRE,
+    OCUPADO,
+    BLOQUEADO,
+    ANULADO,
+    RESERVADO
+}
